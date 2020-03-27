fe9466ccec1a0e8b2d3b91ac1e4111bdc6000ec4
Shrey Upadhyay <62588092+shrey708@users.noreply.github.com>
2020-03-28 00:44:10 +0530

    Updated combination

---
+++ p and c basic.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n) {int count;
+   if (n == 0 || n == 1)
+      return 1;
+   else{
+     
+   return n * fact(n - 1);
+   count ++;}
+}
+int count;
+int main() {
+   int n, r, comb, per; 
+   cout<<"Enter n : ";
+   cin>>n;
+   cout<<"\nEnter r : ";
+   cin>>r;
+   comb = fact(n) / (fact(r) * fact(n-r));
+   cout << "\nCombination : " << comb; 
+   per = fact(n) / fact(n-r);
+   cout << "\nPermutation : " << per;
+   cout<<count;
+   return 0;
+}
